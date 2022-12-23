9cb4afe428c4274f9c46d29756a83471a123240b
oguzhancirak <yakamoz_0519@hotmail.com>
2022-12-24 01:48:55 +0300

    static file upload

--- app.js
+++ app.js
@@ -1,15 +1,35 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+
+// !middleware fonksiyonu üç parametre alır//request ve respondsun arasında bulunan her şey middlwaredir
+// const myLogger = (req,res,next)=>{
+  // console.log("middleware log1")
+  // next();//! eğer next demezsen bir kişi request yaptığında altta kodumuz çalışır ama site sürekli dönder döngüyü tamamlyamaz tamamlaması için next() methodu kullanılır
+// }
+
+// const myLogger2=(req,res,next)=>{
+  // console.log("middleware log2");
+  // next();
+// }
+// 
+
+
+
+//! MİDDLEWARES
 app.use(express.static('public'));
 
+// !middleware çalışıtrmak
+// !middleware yukarıdan aşağı sırayla çalışırlar
+// app.use(myLogger);
+// app.use(myLogger2)
+
+
 app.get('/', (req, res) => {
-  res.send('hello');
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
 });
 
-app.get('/benzin', (req, res) => {
-  res.send('hüüsooooo');
-});
 
 const port = 3000;
 
